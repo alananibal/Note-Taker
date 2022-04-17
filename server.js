0e812a89c6ac9fc151ea75b4f610d9475474e3fa
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-04-16 19:51:47 -0700

    Heroku Deplo 4.16

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 // const { fstat } = require('fs');
-const { notes } = require('./develop/db/db.json');
+const { notes } = require('./db/db.json');
 // const { json } = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -24,15 +26,15 @@ function filterById(id, notesArray) {
 };
 
 // function to create note
-// function createNewNote(body, notesArray) {
-//     const note = body;
-//     notesArray.push(note);
-//     fs.writeFileSync(
-//         path.join(__dirname, './develop/db/db.json'),
-//         JSON.stringify({notes: notesArray }, null, 2)
-//     );
-//     return note;
-// }
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes: notesArray }, null, 2)
+    );
+    return note;
+}
 // function to validate the notes entry
 // need to check Id
 function validateNote(note) {
@@ -50,7 +52,14 @@ function validateNote(note) {
 
 
 // ROUTES
-
+// Index HTML Route
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+// Notes HTML Route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join (__dirname, './public/notes.html'))
+});
 
 // Route to get notes
 app.get('/api/notes', (req, res) =>{
@@ -69,23 +78,18 @@ app.get('/api/notes/:id', (req,res) =>{
 });
 
 // Route to create a note
-// app.post('/api/notes', (req, res) =>{
-//     req.body.id = notes.length.toString();
+app.post('/api/notes', (req, res) =>{
+    req.body.id = notes.length.toString();
 
-//     if(!validateNote(req.body)) {
-//         res.status(400).send('the note is not properly formated.');
-//     } else {
-//         const note = createNewNote(req.body, notes);
-//         res.json(note);
-//     }
-// });
-
-
-
+    if(!validateNote(req.body)) {
+        res.status(400).send('the note is not properly formated.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
 
 // Route to delete notes fetching api/notes/IUid
-
-
 
 // route to edit notes
 
