4a0775044ccfb2e691d5bd0713fb52a2c6f127c1
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-04-17 21:43:31 -0700

    DevelopMerge prep

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-// const { fstat } = require('fs');
+
 const { notes } = require('./db/db.json');
-// const { json } = require('body-parser');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
